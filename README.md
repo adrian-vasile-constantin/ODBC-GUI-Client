aff082719fc67acd25334bcbe2b9c41088bd6b95
Adrian Constantin <adrian.vasile.constantin@gmail.com>
2025-03-07 01:29:16 +0200

    Add UI controls to initial window.
    Populate list of ODBC drivers and Data Source Names.

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 GUI client application for ODBC databases (Qt and python)
 
 Conect to a database or data source using on ODBC connection string or ODBC data source name (DSN), and test and run any SQL queries supported by the data source.
-Some aditional features like listing existing functions, tables and views are also available (some drivers may return an empty list if the product does not support views for example).
 
-If you have the connection details like a database name, hostname, username, etc, you can make a connection string according to the documentation of your database
+Some aditional features like list of existing functions, tables and views are also available (some drivers may return an empty list if the product does not support views for example).
+
+If you have the connection details like a database name, server hostname, username, etc, you can make a connection string according to the documentation of your database
 ODBC driver. The resulting connection string can be saved as a data source name (DSN) for reconnecting later with the same connection information.
 
 All major databases publish an ODBC driver for their product. Microsoft for example makes available ODBC drivers for reading Excel spreadsheets or Access databases,
-SQL Server databases, even the old FoxPro had a driver, but you should have the specific product installed first.
+(included with Office), SQL Server databases, even the old FoxPro had a driver, but you should have the specific product installed first.
 
-Client-server databases will allow to install the ODBC driver (as a client only) without installing the main product (the database server). In this case you can use the
+Client-server databases let you install the ODBC driver (as a client only) without installing the main product (the database server). In this case you can use the
 driver to connect to a remote server, by specifying the server hostname in the connection string.
 
 ## Instalation
@@ -21,13 +22,13 @@ pip install Traits
 pip install pyodbc
 ```
 
-To start the application you should start the odbc-client.py script. On Windows if python is on PATH and is configured to run .py scripts, you can just say
+To start the application you should run the odbc-client.py script. On Windows if python is on PATH and is configured to run .py scripts, you can just say
 ```cmd
 odbc-client
 ```
 from the project directory, or if that does not work use:
 ```cmd
-python .\odbc-client.py
+python odbc-client.py
 ```
 from the main project directory.
 
--- odbc-client.py
+++ odbc-client.py
@@ -1,19 +1,72 @@
 import sys
-from PySide6 import QtCore, QtGui, QtQuick
+from PySide6 import QtGui
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+        QApplication,
+        QMainWindow,
+        QWidget,
+        QGroupBox,
+        QStyleOptionGroupBox,
+        QListWidget,
+        QLabel,
+        QLineEdit,
+        QGridLayout)
 import traits
 import pyodbc
 
+MAIN_WINDOW_WIDTH = 700                 # Windows 10 system requirements include monitor resolution of 800x600
+MAIN_WINDOW_HEIGHT = 525                # Allow 40 - 70 pixels for Windows taskbar
+
+def resizeMainWindow(mainWindow):
+    wndSize = mainWindow.size()
+
+    if wndSize.width() < MAIN_WINDOW_WIDTH:
+        wndSize.setWidth(MAIN_WINDOW_WIDTH)
+
+    if wndSize.height() < MAIN_WINDOW_HEIGHT:
+        wndSize.setHeight(MAIN_WINDOW_HEIGHT)
+
+    mainWindow.resize(wndSize)
+
 def main(argv):
-    for ds in pyodbc.dataSources():
-        print("Data Source: " + ds)
+    mainApp = QApplication(argv)
+    mainWindow = QMainWindow()
+    mainWindow.setCentralWidget(QWidget(mainWindow))
 
-    print("")
+    driverList = QListWidget(mainWindow.centralWidget())
+    dsnList = QListWidget(mainWindow.centralWidget())
 
-    for drv in pyodbc.drivers():
-        print("Driver: " + drv)
+    resizeMainWindow(mainWindow)
 
-    mainApp = QtGui.QGuiApplication(argv)
-    mainWindow = QtQuick.QQuickView()
+    driverList.addItems(pyodbc.drivers())
+    dsnList.addItems(val for key, val in enumerate(pyodbc.dataSources()))
+
+    grid = QGridLayout(mainWindow.centralWidget())
+
+    manualConnectionFrame = QGroupBox(mainApp.tr('Manual connection string'), mainWindow.centralWidget())
+    # manualConnectionFrame.setStyleSheet('QGroupBox { border: 1px solid light-grey; } QGroupBox::title { left: 1ex, top: -0.5ex; }')
+
+    subgrid = QGridLayout(manualConnectionFrame)
+
+    subgrid.addWidget(QLabel(mainApp.tr('New Name:'), manualConnectionFrame), 0, 0)
+    subgrid.addWidget(QLineEdit(manualConnectionFrame), 1, 0)
+    subgrid.setColumnStretch(0, 1)
+
+    subgrid.addWidget(QLabel(mainApp.tr('Connection String:'), manualConnectionFrame), 0, 2)
+    subgrid.addWidget(QLineEdit(manualConnectionFrame), 1, 2)
+    subgrid.setColumnStretch(2, 5)
+
+    grid.addWidget(manualConnectionFrame, 0, 0, 1, 3)
+
+    grid.addWidget(QLabel(mainApp.tr('ODBC Drivers:'), mainWindow.centralWidget()), 1, 0)
+    grid.addWidget(driverList, 2, 0)
+
+    grid.addWidget(QLabel(mainApp.tr('Data Sources:'), mainWindow.centralWidget()), 1, 2)
+    grid.addWidget(dsnList, 2, 2)
+
+    grid.setRowStretch(2, 1)
+    grid.setColumnStretch(0, 1)
+    grid.setColumnStretch(2, 1)
 
     mainWindow.show()
     sys.exit(mainApp.exec())
