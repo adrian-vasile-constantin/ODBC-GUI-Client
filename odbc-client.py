d0d82a2dd07cd6df8f11b276c9004c8dadd0495b
Adrian Constantin <adrian.vasile.constantin@gmail.com>
2025-03-07 03:32:55 +0200

    Add new line of controls.
    Add a Quit button that closes the main window, much like in a dialog.
    Add Uername and Password edit controls.

--- odbc-client.py
+++ odbc-client.py
@@ -10,9 +10,11 @@ from PySide6.QtWidgets import (
         QListWidget,
         QLabel,
         QLineEdit,
+        QPushButton,
         QGridLayout)
 import traits
 import pyodbc
+import keyring
 
 MAIN_WINDOW_WIDTH = 700                 # Windows 10 system requirements include monitor resolution of 800x600
 MAIN_WINDOW_HEIGHT = 525                # Allow 40 - 70 pixels for Windows taskbar
@@ -28,10 +30,20 @@ def resizeMainWindow(mainWindow):
 
     mainWindow.resize(wndSize)
 
+class MainPanel(QWidget):
+    def __init__(self, parentWidget = None):
+        super().__init__(parentWidget)
+
+    def keyPressEvent(self, ev):
+        if ev.key() == Qt.Key_Escape and not ev.modifiers():
+            self.parent().close()
+
+        super().keyPressEvent(ev)
+
 def main(argv):
     mainApp = QApplication(argv)
     mainWindow = QMainWindow()
-    mainWindow.setCentralWidget(QWidget(mainWindow))
+    mainWindow.setCentralWidget(MainPanel(mainWindow))
 
     driverList = QListWidget(mainWindow.centralWidget())
     dsnList = QListWidget(mainWindow.centralWidget())
@@ -48,23 +60,44 @@ def main(argv):
 
     subgrid = QGridLayout(manualConnectionFrame)
 
-    subgrid.addWidget(QLabel(mainApp.tr('New Name:'), manualConnectionFrame), 0, 0)
+    subgrid.addWidget(QLabel(mainApp.tr('New name:'), manualConnectionFrame), 0, 0)
     subgrid.addWidget(QLineEdit(manualConnectionFrame), 1, 0)
     subgrid.setColumnStretch(0, 1)
 
-    subgrid.addWidget(QLabel(mainApp.tr('Connection String:'), manualConnectionFrame), 0, 2)
+    subgrid.addWidget(QLabel(mainApp.tr('Connection string:'), manualConnectionFrame), 0, 2)
     subgrid.addWidget(QLineEdit(manualConnectionFrame), 1, 2)
     subgrid.setColumnStretch(2, 5)
 
     grid.addWidget(manualConnectionFrame, 0, 0, 1, 3)
 
-    grid.addWidget(QLabel(mainApp.tr('ODBC Drivers:'), mainWindow.centralWidget()), 1, 0)
-    grid.addWidget(driverList, 2, 0)
+    subgrid = QGridLayout()
+    subgrid.addWidget(QLabel(mainApp.tr('Username:'), mainWindow.centralWidget()), 0, 0)
+    subgrid.addWidget(QLineEdit(mainWindow.centralWidget()), 1, 0)
 
-    grid.addWidget(QLabel(mainApp.tr('Data Sources:'), mainWindow.centralWidget()), 1, 2)
-    grid.addWidget(dsnList, 2, 2)
+    subgrid.addWidget(QLabel(mainApp.tr('Password:'), mainWindow.centralWidget()), 0, 2)
+    subgrid.addWidget(QLineEdit(mainWindow.centralWidget()), 1, 2)
 
-    grid.setRowStretch(2, 1)
+    connectButton = QPushButton(mainApp.tr("Connect"), mainWindow.centralWidget())
+    connectButton.default = True
+    quitButton = QPushButton(mainApp.tr("Quit"), mainWindow.centralWidget())
+
+    quitButton.pressed.connect(lambda: mainWindow.close())
+
+    subgrid.addWidget(connectButton, 1, 4)
+    subgrid.addWidget(quitButton, 1, 6)
+    subgrid.setColumnStretch(0, 1)
+    subgrid.setColumnStretch(2, 1)
+    subgrid.setColumnStretch(3, 2)
+
+    grid.addLayout(subgrid, 1, 0, 1, 3)
+
+    grid.addWidget(QLabel(mainApp.tr('ODBC Drivers:'), mainWindow.centralWidget()), 2, 0)
+    grid.addWidget(driverList, 3, 0)
+
+    grid.addWidget(QLabel(mainApp.tr('Data Sources:'), mainWindow.centralWidget()), 2, 2)
+    grid.addWidget(dsnList, 3, 2)
+
+    grid.setRowStretch(3, 1)
     grid.setColumnStretch(0, 1)
     grid.setColumnStretch(2, 1)
 
